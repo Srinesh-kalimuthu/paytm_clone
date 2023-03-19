84d660bb33d4f7a8be46430173c99639f6d79bab
Srinesh-Kalimuthu <727621bec063@mcet.in>
2023-03-19 13:15:54 +0530

    second commit

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+const sbtImg = document.getElementById("pic");
+const onHover = (e)=>(sbtImg.src="https://pwebassets.paytm.com/commonwebassets/paytmweb/header/images/loginImg.svg");
+const onLeave = (e)=>(sbtImg.src="https://pwebassets.paytm.com/commonwebassets/paytmweb/header/images/logoutImg.svg");
